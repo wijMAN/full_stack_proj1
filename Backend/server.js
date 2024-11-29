37c7baad3b183aab6b07fa5b8a0ec19e33d7ed60
Wijdan Ar <wa5755@protonmail.com>
2024-11-29 06:47:01 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+
+const app = express();
+
+app.get( 
+    '/',(req, res)=> {
+        res.send('server is ready');
+    }
+
+);
+
+app.get(
+    `/ye`,(req,res)=>{
+        res.send(
+            [
+                {
+                    id: 1,
+                    title: 'One'
+                },
+
+                {
+                    id: 2,
+                    title: 'Two'
+                }
+            ]
+        );
+    }
+);
+
+
+
+
+const port = process.env.PORT || 3000; 
+
+app.listen(
+    port,()=>{
+        console.log(`server is running at http://localhost:${port}`);
+    }
+);
